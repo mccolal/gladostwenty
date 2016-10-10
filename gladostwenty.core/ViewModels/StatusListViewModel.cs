41ab3fdff283df18e89f9caf8e9549ca41dc4745
Bertrand Chapman <bertrand_chapman@hotmail.com>
2016-10-11 06:09:48 +1000

    Resources for status page

---
+++ gladostwenty.core/ViewModels/StatusListViewModel.cs
@@ -0,0 +1,37 @@
+﻿using gladostwenty.core.Models;
+using gladostwenty.core.Services;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gladostwenty.core.ViewModels {
+    public class StatusListViewModel : MvxViewModel{
+
+        private ObservableCollection<Status> statuses;
+
+        public ObservableCollection<Status> Statuses {
+            get {
+                return statuses;
+            }
+            set {
+                SetProperty(ref statuses, value);
+            }
+        }
+
+        public StatusListViewModel() {
+
+            InitializeList();
+        }
+
+        private async void InitializeList() {
+
+            var dataService = Mvx.Resolve<IAzureDataService>();
+            Statuses = new ObservableCollection<Status>(await dataService.GetStatusTable());
+        }
+    }
+}
