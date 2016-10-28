a834adfbac2e2c4d7705924575624252387e1080
Alexander McColl <mccolal@gmail.com>
2016-10-28 21:58:45 +1000

    i did ittttt

---
+++ gladostwenty.droid/Bootstrap/DownloadCachePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace gladostwenty.droid.Bootstrap
+{
+    public class DownloadCachePluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.DownloadCache.PluginLoader>
+    {
+    }
+}
