8c466e59916333012498cafe67159168669dc114
Alexander McColl <mccolal@gmail.com>
2016-11-02 12:50:13 +1000

    icons

--- gladostwenty.core/ViewModels/NotificationListViewModel.cs
+++ gladostwenty.core/ViewModels/NotificationListViewModel.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using static Android.Resource;
 
 namespace gladostwenty.core.ViewModels
 {
@@ -108,10 +109,11 @@ namespace gladostwenty.core.ViewModels
                 {
                     if (status.Seen)
                     {
-                        ImageUrl.imgurl = "https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-open.png";
+                        ImageUrl.imgurl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Small-dark-grey-circle.svg/2000px-Small-dark-grey-circle.svg.png";
+
                     } else
                     {
-                        ImageUrl.imgurl = "https://cdn2.iconfinder.com/data/icons/ui-1/57/18-512.png";
+                        ImageUrl.imgurl = "http://bluedot.ca/wp-content/themes/dsf-blue-dot-campaign-theme/src/images/marker-circle.png";
                     }
                     stats.Add(new StatusListItem { Status = status, Contact = await Mvx.Resolve<IAzureDataService>().GetUser(status.FromId), imgUrl = ImageUrl });
                 }
@@ -119,6 +121,17 @@ namespace gladostwenty.core.ViewModels
             StatusList = new ObservableCollection<StatusListItem>(stats);
             Loading = false;
         }
+        private Drawable _myDrawable;
+        public Drawable MyDrawable
+        {
+            get { return _myDrawable; }
+            set
+            {
+                _myDrawable = value;
+                RaisePropertyChanged(() => MyDrawable);
+            }
+        }
+
     }
 
 
